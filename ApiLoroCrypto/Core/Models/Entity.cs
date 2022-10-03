31e2f841e5d9964a883239b61e9c4e39a1faabf4
lucas ludueña <lucas.ludu.mdp@hotmail.com>
2022-10-03 20:34:35 -0300

    Entity User

---
+++ ApiLoroCrypto/Core/Models/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiLoroCrypto.Core.Models
+{
+    public class Entity
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
