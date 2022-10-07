98f022b6fb47da51de585814d90755caa5342c0f
sergio351 <velazquezsergio714@gmail.com>
2022-10-07 20:24:00 -0300

    probandologin

---
+++ ApiLoroCrypto/Core/Helper/ApiHelper.cs
@@ -0,0 +1,39 @@
+﻿using ApiLoroCrypto.Core.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ApiLoroCrypto.Core.Helper
+{
+    public static class ApiHelper
+    {
+        public static string GetSHA256(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+       
+        }
+
+        public static User ValidateUser(User user, string password)
+        {
+            string encriptedPassword = GetSHA256(password);
+            if (user.Password != encriptedPassword)
+            {
+                return null;
+            }
+            return user;
+        }
+
+
+
+    }
+
+
+}
---
+++ ApiLoroCrypto/Repository/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using ApiLoroCrypto.DataAccess;
+using ApiLoroCrypto.Repository.Interfaces;
+
+namespace ApiLoroCrypto.Repository
+{
+    public class UnitOfWork : IUnitofWork
+    {
+        private readonly ApplicationDbContext _context;
+        public IUserRepository UserRepository { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            UserRepository = new UserRepository(context);    
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
