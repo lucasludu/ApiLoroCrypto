ad779ea1831ac447d91421fea3518b40c6c92fc5
lucas ludueña <lucas.ludu.mdp@hotmail.com>
2022-10-10 14:32:01 -0300

    Add Swagger and JWT

--- ApiLoroCrypto/Controllers/AuthController.cs
+++ ApiLoroCrypto/Controllers/AuthController.cs
@@ -1,13 +1,12 @@
 ﻿using ApiLoroCrypto.Core.Dto;
 using ApiLoroCrypto.Core.DTO;
-using ApiLoroCrypto.Core.Services;
 using ApiLoroCrypto.Core.Services.Interfaces;
 using AutoMapper;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiLoroCrypto.Controllers
 {
+    [ApiExplorerSettings(GroupName = "ApiAuth")]
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
@@ -20,6 +19,12 @@ namespace ApiLoroCrypto.Controllers
             _service = service;
             _mapper = mapper;
         }
+
+        /// <summary>
+        /// Register new User
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
         [HttpPost("Register")]
         public async Task<IActionResult> Register(UserRegisterDto dto)
         {
@@ -32,6 +37,11 @@ namespace ApiLoroCrypto.Controllers
             return Ok(user);
         }
 
+        /// <summary>
+        /// Login User
+        /// </summary>
+        /// <param name="userLoginDto"></param>
+        /// <returns></returns>
         [HttpPost("Login")]
         public async Task<IActionResult> Login([FromBody] UserLoginDto userLoginDto)
         {
--- ApiLoroCrypto/Program.cs
+++ ApiLoroCrypto/Program.cs
@@ -4,7 +4,12 @@ using ApiLoroCrypto.Core.Services.Interfaces;
 using ApiLoroCrypto.DataAccess;
 using ApiLoroCrypto.Repository;
 using ApiLoroCrypto.Repository.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(Options => {
     Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
 builder.Services.AddScoped<IUnitofWork, UnitOfWork>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IUserService, UserService>();
@@ -20,7 +26,73 @@ builder.Services.AddAutoMapper(typeof(MapperProfile));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+#region JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(option =>
+{
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        //ValidIssuer = builder.Configuration["Jwt:Issuing"],
+        //ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))//secreto del appsetting.json
+    };
+});
+#endregion JWT
+
+
+#region Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("ApiAuth", new OpenApiInfo
+    {
+        Title = "Api Auth",
+        Version = "1",
+        Description = "Backend Chat"
+    });
+    c.SwaggerDoc("ApiUser", new OpenApiInfo
+    {
+        Title = "Api User",
+        Version = "1",
+        Description = "Backend Chat"
+    });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+
+    c.AddSecurityDefinition("Bearer",
+        new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+#endregion
+
 
 var app = builder.Build();
 
@@ -28,10 +100,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/ApiAuth/swagger.json", "Api Auth");
+        c.SwaggerEndpoint("/swagger/ApiUser/swagger.json", "Api User");
+        c.DefaultModelsExpandDepth(-1);
+    });
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
